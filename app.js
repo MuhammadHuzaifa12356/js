0a35ec3f9e8beda02aa5fc8d4496ff6057360642
MuhammadHuzaifa12356 <huzaifajt2525@gmail.com>
2022-11-02 17:15:24 +0500

    Assignment 1

---
+++ app.js
@@ -0,0 +1,131 @@
+// chpater 1 start
+
+// alert("THANKS FOR INPUT");
+// alert ( "Thanks for your input!" );
+
+// chpater 1 end
+// //////////////////////////////////////////////////////////////////////////
+// chpater 2 start
+var name = "Mark";
+name = "Ace";
+console.log(name);
+var nationality;
+nationality = "U.S.";
+console.log(nationality);
+var floogle = "Mark";
+console.log(floogle);
+ var thanx = "Thanks for your input!"
+//  alert(thanx);
+ var nickname = "Bub";
+console.log(nickname)
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";;
+console.log(lastName)
+console.log(cityOfOrigin)
+console.log(aussieGreeting)
+// chpater 2 end
+// //////////////////////////////////////////////////////////////////////////
+// chpater 3 start
+var weight = 150;
+weight +25;
+console.log(weight)
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(newNum);
+var originalNum = 23;
+ var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log(newNum);
+var originalNum = 90;
+ originalNum = originalNum + 10;
+console.log(originalNum);
+var originalNum = "23";
+ var newNum = originalNum + 7;
+//  var 1stPresident = "Washington"; // illegal
+ console.log(newNum);
+ var caseQty = 144;
+ alert(caseQty);
+// chpater 3 end
+// //////////////////////////////////////////////////////////////////////////
+// chpater 4 start
+// var abc //legal
+// var 123 //illegal starting ch should not be number
+// var ab12 //legal 
+// var 12ab //illegal
+// var var //illegal , 
+// key words that are stored in java cant be use
+// var a+b // illegal ,arithmatic operators cant be use
+// chpater 4 end
+// //////////////////////////////////////////////////////////////////////////
+
+// chpater 5 start
+
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+console.log(popularNumber);
+// alert(2 + 2);
+var popularNumber = 12 - 24;
+// console.log(popularNumber);
+var popularNumber = 3 * 12;
+// console.log(popularNumber);
+var num = 10;
+ var anotherNum = 1;
+var popularNumber = num + anotherNum
+console.log(popularNumber);
+var whatsLeftOver = 10 % 3;
+console.log(whatsLeftOver);
+var popularNumber = 3 * 12;
+// console.log(popularNumber);
+var whatsLeftOver = 9 % 3;
+// console.log(whatsLeftOver);
+// chpater 5 end
+// //////////////////////////////////////////////////////////////////////////
+
+// chap 6 start
+num++;
+num = num + 1;
+num--;
+var num = 1;
+ var newNum = ++num;
+//  console.log(newNum);
+ var num = 1;
+var newNum = num--;
+var num = 1;
+ var newNum = num--;
+//  console.log(newNum);
+
+// chap 6 end
+// //////////////////////////////////////////////////////////////////////////
+// chap 7 start
+var totalCost = 1 + 3 * 4;
+var totalCost = 1 + (3 * 4);
+console.log(totalCost);
+var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation);
+resultOfComputation = ((2 * 4) * 4) + 2;
+// console.log(resultOfComputation);
+resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(resultOfComputation);
+
+// chap 7 end
+// //////////////////////////////////////////////////////////////////////////
+// chap 8 start
+
+var message = "Thanks for your input!";
+ alert(message);
+ var username="huzaifa";
+//  alert("Thanks, " + userName + "!");
+alert(username);
+// var message = "Thanks, ";
+//  var banger = "!";
+//  alert(message + userName + banger);
+//  var message = "Thanks, ";
+//  var userName = "Susan";
+//  var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess);
+// alert("2" + "2");
+alert("2 plus 2 equals " + 2 + 2);
+
+// chap 8 end
